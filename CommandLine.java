eaf82ae26062de59d886f5e7685298cf283f70e5
Diego2941 <diego2941@hotmail.com>
2019-07-24 19:45:21 -0400

    Added create account

--- CommandLine.java
+++ CommandLine.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 public class CommandLine {
 	private Scanner sc = null;
 	private SQLop sql = null;
+	private String adminname = "Admin";
+	private String adminpassword = "password";
+	private String username = "";
 	
 	public boolean startSession() {
 		boolean success = true;
@@ -39,37 +42,54 @@ public class CommandLine {
 	
 	private String[] getCredentials() {
 		String[] cred = new String[3];
+		cred[0] = "root";
+		cred[1] = "root";
+		cred[2] = "cscc43db";
+		return cred;
+	}
+	
+	private String[] userlogin() {
+		String[] cred = new String[2];
 		System.out.print("Username: ");
 		cred[0] = sc.nextLine();
 		System.out.print("Password: ");
 		cred[1] = sc.nextLine();
-		System.out.print("Database: ");
-		cred[2] = sc.nextLine();
+		return cred;
+	}
+	
+	private String[] adminlogin() {
+		String[] cred = new String[2];
+		System.out.print("Username: ");
+		cred[0] = sc.nextLine();
+		System.out.print("Password: ");
+		cred[1] = sc.nextLine();
 		return cred;
 	}
 	
 	public boolean execute() {
 		if (sc != null && sql != null) {
-			System.out.println("");
-			System.out.println("***************************");
-			System.out.println("******ACCESS GRANTED*******");
-			System.out.println("***************************");
-			System.out.println("");
-			
+			System.out.println("\n*****************************");
+			System.out.println("******Welcome to MyBnB*******");
+			System.out.println("*****************************\n");
+
 			String input = "";
 			 while(! input.equalsIgnoreCase("exit")) {
 				 System.out.println("Please enter which operations to perform:"
-		            		+ "user(User operation), reports(Admin operations)"
+		            		+ "sigup(Create account)"
+						 	+ "login(User operation), "
+		            		+ "reports(Admin operations) "
 		            		+ "or exit(exit the program).");
 		            input = sc.nextLine();
 		            switch(input) {
-		            	case "users":
+		            	case "signup":
+		            		break;
+		            	case "login":
 		            		break;
 		            	case "reports":
 		            		reportOperation();
 		            		break;
 		            	case "exit":
-		                    System.out.println("GoodBye!");
+		                    System.out.println("Thakns for visiting!");
 		                    break;
 		            	default:
 		            		System.out.println("Invalid operation. Please try again!");
@@ -85,10 +105,67 @@ public class CommandLine {
 		}
 	}
 	
+	public void userOperation() {
+	      // ask for inputs
+	      String line = "";
+	      System.out.println("Hi, " + username + " ! "
+	              + "Please enter which operations to perform:\n"
+	              + "1. (create account)\n"
+	              + "2. (signin an existing account)\n"
+	              + "3. (create booking)\n"
+	              + "4. (cancel booking)\n"
+	              + "5. (change price)\n"
+	              + "6. (update price)\n"
+	              + "7. (change availability)\n"
+	              + "8. (comment)\n"
+	              + "9. (rate)\n"
+	              + "10. (back to previous page):");
+	      while(! line.equalsIgnoreCase("exit")) {
+	        line = sc.nextLine();
+	          switch(line) {
+	          case "1":
+	              
+	              break;
+	          case "2":
+	              
+	              break;
+	          case "3":
+	              
+	              break;
+	          case "4":
+	              
+	              break;
+	          case "5":
+	              
+	              break;
+	          case "6":
+	              
+	              break;
+	          case "7":
+	              
+	              break;
+	          case "8":
+	              
+	              break;
+	          case "9":
+	              break;
+	          case "10":
+	              break;
+	          case "exit":
+	        	  System.out.println("Signing out.");
+	        	  username = "";
+	              break;
+	          default:
+	              System.out.println("Invalid report. Please try again!");
+	              break;
+	          }
+	      }
+	   }
+	
 	 public void reportOperation() {
 	      // ask for inputs
 	      String line = "";
-	      System.out.println("Hi, Brother Diego! "
+	      System.out.println("Hi, Admin! "
 	              + "Please enter which operations to perform:\n"
 	              + "1. (bookings in specified date range by city)\n"
 	              + "2. (bookings by postal code in specified city)\n"
@@ -141,5 +218,77 @@ public class CommandLine {
 	              break;
 	          }
 	      }
-	    }
+	 }
+	 
+	 public  void initiateTables() throws Exception {
+		 String listing, user, booking, calendar;
+         listing = "CREATE TABLE IF NOT EXISTS "
+                    + "listings(lid int NOT NULL AUTO_INCREMENT, "
+                    + "name varchar(255), "
+                    + "hid int, "
+                    + "ltype varchar(255), "
+                    + "adress varchar(255), "
+                    + "city varchar(255), "
+                    + "postcode int, "
+                    + "country varchar(255), "
+                    + "latitude float, "
+                    + "longitude float, "
+                    + "aid int, "
+                    + "PRIMARY KEY (lid))";
+            
+        user = "CREATE TABLE IF NOT EXISTS "
+                    + "user(uid int NOT NULL AUTO_INCREMENT, "
+                    + "name varchar(255), "
+                    + "password varchar(16), "
+                    + "utype bool, "
+                    + "uaddress varchar(255), "
+                    + "birth date, "
+                    + "ocupation varchar(255), "
+                    + "sin int(9), "
+                    + "payment varchar(255), "
+                    + "PRIMARY KEY (uid))";
+                    
+        booking = "CREATE TABLE IF NOT EXISTS "
+                    + "booking(bid int NOT NULL AUTO_INCREMENT, "
+                    + "lid int, "
+                    + "uid int, "
+                    + "checkin datetime, "
+                    + "checkout datetime, "
+                    + "cancelation bool, "
+                    + "hostcomment varchar(255), "
+                    + "rentercomment varchar(255), "
+                    + "PRIMARY KEY (bid))";
+            
+        calendar = "CREATE TABLE IF NOT EXISTS "
+                    + "calendar(lid int, "
+                    + "startdate date, "
+                    + "enddate date, "
+                    + "price float)";
+            
+        sql.createtable(listing);
+        sql.createtable(user);
+        sql.createtable(booking);
+        sql.createtable(calendar);
+    }
+	    
+	public String[] createAccount() {
+		String[] cred = new String[8];
+		System.out.print("Name: ");
+		cred[0] = sc.nextLine();
+		System.out.print("Password: ");
+		cred[1] = sc.nextLine();
+		System.out.print("UserType: ");
+		cred[2] = sc.nextLine();
+		System.out.print("Address: ");
+		cred[3] = sc.nextLine();
+		System.out.print("Day of birth: ");
+		cred[4] = sc.nextLine();
+		System.out.print("Occupation: ");
+		cred[5] = sc.nextLine();
+		System.out.print("SIN: ");
+		cred[6] = sc.nextLine();
+		System.out.print("Payment Info: ");
+		cred[7] = sc.nextLine();
+		return cred;
+	}
 }
