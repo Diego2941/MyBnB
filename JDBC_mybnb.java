fdfcca2937f499233af6839a738af010edf36e61
chengyu66 <1998xinchengyu@gmail.com>
2019-07-24 17:32:51 -0400

    basic report operation

--- JDBC_mybnb.java
+++ JDBC_mybnb.java
@@ -25,6 +25,7 @@ public class JDBC_mybnb {
                 case "user":
                     break;
                 case "report":
+                  reportOperation();
                     break;
                 case "exit":
                   System.out.println("GoodBye!");
@@ -36,6 +37,62 @@ public class JDBC_mybnb {
         }
         
         br.close();
+    }
+    
+    public static void reportOperation() throws Exception {
+      // ask for inputs
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      String line = "";
+      System.out.println("Hi, Brother Diego! "
+              + "Please enter which operations to perform:\n"
+              + "1. (bookings in specified date range by city)\n"
+              + "2. (bookings by postal code in specified city)\n"
+              + "3. (listings by country)\n"
+              + "4. (listings by country and city)\n"
+              + "5. (listings by country, city and postal code)\n"
+              + "6. (host list ranked by number of listings per country)\n"
+              + "7. (host list ranked on number of listings by city)\n"
+              + "8. (commercial hosts by country and city)\n"
+              + "9. (report renters by number of bookings within a specified date)\n"
+              + "10. (report renters by number of bookings within a specified date grouped by city)\n"
+              + "11. (report hosts with the largest number of cancellation within a specified year)\n"
+              + "12. (back to previous page):");
+      while(! line.equalsIgnoreCase("q")) {
+        line = br.readLine();
+          switch(line) {
+          case "1":
+              
+              break;
+          case "2":
+              
+              break;
+          case "3":
+              
+              break;
+          case "4":
+              
+              break;
+          case "5":
+              
+              break;
+          case "6":
+              
+              break;
+          case "7":
+              
+              break;
+          case "8":
+              
+              break;
+          case "9":
+              break;
+          case "10":
+              break;
+          default:
+              System.out.println("Invalid report. Please try again!");
+              break;
+          }
+      }
     }
     
     public static Connection getConnection() throws Exception{
